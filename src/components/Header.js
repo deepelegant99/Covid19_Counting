1b9eb9ea5246ab028f6e840d0edfac751b825dfa
Henry Crossley <crossh2001@yahoo.com>
2021-10-01 00:13:54 -0700

    added button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button"
+import { borderRadius } from "@mui/system";
+
+const useStyles = makeStyles({
+  Button: {
+      width: 100,
+      height: 50,
+      backgroundColor:"gray",
+      borderRadius:2
+  },
+});
+
 
 const Header = ()=>{
     return (
       <>
-        <Link to="/">Main</Link>
-        <Link to="/CountryPage">Country</Link>
-        <Link to="/ContinentPage">Continent</Link>
-        <Link to="/LocationPage">Location</Link>
-        <Link to="/GenericCountry">Generic Country</Link>
+        <Link to="/"><Button>Main</Button></Link>
+        <Link to="/CountryPage"><Button>Country</Button></Link>
+        <Link to="/ContinentPage"><Button>Continent</Button></Link>
+        <Link to="/LocationPage"><Button>Location</Button></Link>
+        <Link to="/GenericCountry"><Button>Generic Country</Button></Link>
       </>
     );
 }
