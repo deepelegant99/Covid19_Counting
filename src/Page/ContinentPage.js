3d0689989df332e0d9c8b5e57fb942b7decb2c49
Henry Crossley <crossh2001@yahoo.com>
2021-10-02 00:39:37 -0700

    played with Continent Data

--- src/Page/ContinentPage.js
+++ src/Page/ContinentPage.js
@@ -1,8 +1,27 @@
-import DisplayItem from '../components/DisplayItems'
+import DisplayItem from '../components/DisplayItems';
+import axios from "axios";
+import { useEffect } from "react";
+
 
 const ContinentPage = ({data}) => {
 
-    console.log(data['']);
+    const baseUrl = "https://covid-api.mmediagroup.fr/v1/cases?continent=Europe";
+
+    const testData={};
+
+    useEffect(() => {
+      axios
+        .get(baseUrl)
+        .then((response) =>
+          //Do something with the data
+          testData
+        )
+        .catch((error) => console.error(`${error}`));
+    }, []);
+  
+
+
+    console.log(testData);
 
 return(
         <DisplayItem data={data} />
