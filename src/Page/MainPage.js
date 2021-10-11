8985e8c9c69b49dad90ef4a21a1b009ef25b51fd
Henry Crossley <crossh2001@yahoo.com>
2021-10-10 22:04:34 -0700

    added a globe to main page

--- src/Page/MainPage.js
+++ src/Page/MainPage.js
@@ -2,7 +2,11 @@
 
 const MainPage = () => {
 
-return(<h1>Main Page</h1>)
+return(<center>
+    
+    <img src="https://cdn11.bigcommerce.com/s-5ig7x53cx8/images/stencil/600x600/products/2358/2222/ME-2492-Globe-B__08333.1539612660.png?c=2"
+    width="700px" height="700px"/>
+    </center>)
 
 }
 
